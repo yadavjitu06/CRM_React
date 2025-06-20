e69dc7c9c6a99cb41211efbb7a5c7b46cba0550d
yadavjitu06 <120108392+yadavjitu06@users.noreply.github.com>
2025-06-21 00:44:20 +0530

    integrated daisyui plugin with night theme in our app

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),],
 });
