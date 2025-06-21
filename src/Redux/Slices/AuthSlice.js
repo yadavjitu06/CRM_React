ba745e594faa09f558fac4ea9bc8702f5483c8bd
yadavjitu06 <120108392+yadavjitu06@users.noreply.github.com>
2025-06-22 00:43:32 +0530

    Added Auth slice

---
+++ src/Redux/Slices/AuthSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+
+const initialState={
+    role:localStorage.getItem("role")||"",
+    data:localStorage.getItem("data")||{},
+    isLoggedIn:localStorage.getItem("isLoggedIn")||false
+}
+const authSlice = createSlice({
+name :"auth",
+initialState,
+reducers:{}
+})
+
+
+export default authSlice.reducer
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authSliceReducer from '../Redux/Slices/AuthSlice'
+const store = configureStore({
 
-const store =configureStore({
-
-    reducer:{},
+    reducer:{
+        auth:authSliceReducer
+    },
     devTools:true
 
 });
