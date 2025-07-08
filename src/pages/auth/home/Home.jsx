b25fdb39ec4f08076f30d8511be46dac608012d6
yadavjitu06 <120108392+yadavjitu06@users.noreply.github.com>
2025-07-08 15:17:13 +0530

    highly reusable card component created

--- src/pages/auth/home/Home.jsx
+++ src/pages/auth/home/Home.jsx
@@ -1,11 +1,33 @@
-
+import { BsFillPencilFill } from "react-icons/bs";
+import Card from "../../../components/Card.Jsx";
 import Homelayout from "../../../layouts/Homelayout";
 
 function Home() {
-  return(
-  <Homelayout>
-
-  </Homelayout>
-  )
+  return (
+    <Homelayout>
+      <Card
+        titleText="Open"
+        status={30}
+        // quantity=" "
+        background="bg-yellow-300"
+        borderColor="border-green-300"
+        fontColor="text-black"
+        dividerColor="bg-black"
+      >
+        <BsFillPencilFill className="inline mr-2" />
+      </Card>
+      {/* <Card
+        titleText="Open"
+        status={30}
+        quantity={30}
+        background="bg-yellow-300"
+        borderColor="border-green-300"
+        fontColor="text-black"
+        dividerColor="bg-black"
+      >
+        <BsFillPencilFill className="inline mr-2" />
+      </Card> */}
+    </Homelayout>
+  );
 }
 export default Home;
