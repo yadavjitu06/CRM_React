9cd8155d250b68a2b10a9b687a09c6611485b214
yadavjitu06 <120108392+yadavjitu06@users.noreply.github.com>
2025-07-10 15:31:20 +0530

    Added ticketslice for miantaing ticet state

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,10 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import authSliceReducer from '../Redux/Slices/AuthSlice'
+import ticketSliceReducer from '../Redux/Slices/TicketSlice'
+
 const store = configureStore({
 
     reducer:{
-        auth:authSliceReducer
+        auth:authSliceReducer,
+        tickets:ticketSliceReducer
     },
      middleware: (getDefaultMiddleware) => getDefaultMiddleware({serializableCheck: false}),
     devTools: true
--- src/pages/auth/home/Home.jsx
+++ src/pages/auth/home/Home.jsx
@@ -1,8 +1,23 @@
 import { BsFillPencilFill } from "react-icons/bs";
 import Card from "../../../components/Card.Jsx";
 import Homelayout from "../../../layouts/Homelayout";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllTicketsforTheUser } from "../../../Redux/Slices/TicketSlice";
 
 function Home() {
+  const authState = useSelector((state) => state.auth);
+  // const ticketState = useSelector((state) => state.tickets);
+  const dispatch = useDispatch();
+  async function loadTickets() {
+    const responce = await dispatch(getAllTicketsforTheUser());
+
+    console.log(responce);
+  }
+
+  useEffect(() => {
+    loadTickets();
+  }, [authState.token]);
   return (
     <Homelayout>
       <Card
